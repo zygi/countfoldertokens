dda3a23b5d6343e9604df7753a75a316d235281e
Zygimantas Straznickas <hi@zygi.me>
2024-02-15 21:46:55 -0800

    Fix startup

--- countfoldertokens/__init__.py
+++ countfoldertokens/__init__.py
@@ -13,6 +13,7 @@ parser.add_argument('folder', type=str, help='folder to count tokens in')
 parser.add_argument('--pattern', type=str, required=False, help='pattern to match files against')
 parser.add_argument('--tokenizer', type=str, default="cl100k_base", help='tokenizer to use')
 parser.add_argument('-p', '--progress', action=argparse.BooleanOptionalAction, default=True, help='show a progress bar')
+parser.add_argument('--respect-gitignore', action=argparse.BooleanOptionalAction, default=True, help='respect .gitignore files')
 parser.add_argument('-v', '--verbose', action=argparse.BooleanOptionalAction, help='print information about processed files')
 
 def initializer(tok_name):
@@ -28,6 +29,16 @@ def token_count(file):
     except UnicodeDecodeError:
         return (file, None)
 
+def filter_gitignore(start_folder, files):
+    import git
+    repo = git.Repo(start_folder, search_parent_directories=True)
+    ignored = set(repo.ignored(files))
+    # print(ignored)
+    # print([str(f) for f in files])
+    notignored = {f for f in files if str(f) not in ignored}
+    repo.close()
+    return notignored
+    
 def main():
     args = parser.parse_args()
     folder = args.folder
@@ -35,6 +46,11 @@ def main():
     
     # get file tree recursively
     files = list(e for e in pathlib.Path(folder).glob("**/*" if args.pattern is None else args.pattern) if e.is_file())
+    
+    if args.respect_gitignore:
+        files = filter_gitignore(folder, files)
+        # also filter out .git directories
+        files = [f for f in files if not ".git" in f.parts and f.name != ".gitignore"]
     
     with concurrent.futures.ProcessPoolExecutor(initializer=initializer, initargs=(args.tokenizer,)) as executor:
         context = tqdm.tqdm(total=len(files)) if args.progress else contextlib.nullcontext()
