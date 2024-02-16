463842b1bacd9a747f70cbecb0185def5f2b2c8f
Zygimantas Straznickas <hi@zygi.me>
2024-02-15 21:35:08 -0800

    Fix startup

--- countfoldertokens/__init__.py
+++ countfoldertokens/__init__.py
@@ -12,7 +12,8 @@ parser = argparse.ArgumentParser(description='Count tokens in a folder')
 parser.add_argument('folder', type=str, help='folder to count tokens in')
 parser.add_argument('--pattern', type=str, required=False, help='pattern to match files against')
 parser.add_argument('--tokenizer', type=str, default="cl100k_base", help='tokenizer to use')
-parser.add_argument('-q', '--quiet', type=bool, default=False, help='tokenizer to use')
+parser.add_argument('-p', '--progress', action=argparse.BooleanOptionalAction, default=True, help='show a progress bar')
+parser.add_argument('-v', '--verbose', action=argparse.BooleanOptionalAction, help='print information about processed files')
 
 def initializer(tok_name):
     global enc
@@ -20,9 +21,12 @@ def initializer(tok_name):
     
 def token_count(file):
     global enc
-    with open(file, "r") as f:
-        text = f.read()
-    return len(enc.encode(text))
+    try:
+        with open(file, "r") as f:
+            text = f.read()
+        return (file, len(enc.encode(text)))
+    except UnicodeDecodeError:
+        return (file, None)
 
 def main():
     args = parser.parse_args()
@@ -32,16 +36,19 @@ def main():
     # get file tree recursively
     files = list(e for e in pathlib.Path(folder).glob("**/*" if args.pattern is None else args.pattern) if e.is_file())
     
-    
     with concurrent.futures.ProcessPoolExecutor(initializer=initializer, initargs=(args.tokenizer,)) as executor:
-        context = contextlib.nullcontext() if args.quiet else tqdm.tqdm(total=len(files))
+        context = tqdm.tqdm(total=len(files)) if args.progress else contextlib.nullcontext()
         with context as pbar:
             futures = {executor.submit(token_count, file): file for file in files}
             tok_counts = []
             for future in concurrent.futures.as_completed(futures):
-                tok_counts.append(future.result())
-                if not args.quiet:
+                filename, tok_count = future.result()
+                if tok_count is not None:
+                    tok_counts.append(tok_count)
+                if args.progress:
                     pbar.update(1)
+                if args.verbose:
+                    print(f"{filename}: {tok_count}")
     
     print(sum(tok_counts))
 
